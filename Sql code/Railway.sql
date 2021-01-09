1402f57acb49ee46888ecadfd1cf6c89cfc87e52
NaderYouhanna <naderyouhanna@gmail.com>
2021-01-09 20:34:37 +0200

    Quantity NOT NULL

--- Sql code/Railway.sql
+++ Sql code/Railway.sql
@@ -121,7 +121,7 @@ CREATE TABLE INVENTORY
 (
   Station_Number INT NOT NULL,
   Part_Number INT NOT NULL,
-  Quantity INT,
+  Quantity INT NOT NULL,
   FOREIGN KEY (Station_Number) REFERENCES STATION( Station_Number),
   FOREIGN KEY (Part_Number) REFERENCES SPARE_PART (Part_Number),
   PRIMARY KEY (Station_Number,Part_Number)
